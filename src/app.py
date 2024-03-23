66de47f530df8145d9053bf5856b200e3d808ae7
Abhijeet Dewangan <02limzadewangan@gmail.com>
2024-03-23 22:11:01 +0530

    Update-e-app.py

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 
-def addition(a,b):
-    return a+b
-def subtraction(a,b):
-    return a-b
+def addition(a,be):
+    return a+b+e
+def subtraction(a,b,e):
+    return a-b-e
 def division(a,b):
     return a/b 
 
